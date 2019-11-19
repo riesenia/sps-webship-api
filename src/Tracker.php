f438ab3a776aebf2522f209d377ed5360bca436b
Tomas Saghy <tomas.saghy@gmail.com>
2019-11-19 16:08:26 +0100

    getStatusHistory

--- src/Tracker.php
+++ src/Tracker.php
@@ -37,6 +37,33 @@ class Tracker
     }
 
     /**
+     * Get shipment status history by shipment number.
+     *
+     * @param string $number
+     *
+     * @return \stdClass[]
+     */
+    public function getStatusHistory(string $number): array
+    {
+        if (!\preg_match('/([0-9]{3})-([0-9]{3})-([0-9]+)/', $number, $m)) {
+            throw new \InvalidArgumentException('Invalid shipment number format!');
+        }
+
+        try {
+            $response = $this->soap->__call('getParcelStatus', [
+                'landnr' => $m[1],
+                'mandnr' => $m[2],
+                'lfdnr' => $m[3],
+                'langi' => $this->language
+            ]);
+        } catch (\SoapFault $e) {
+            return [];
+        }
+
+        return $response;
+    }
+
+    /**
      * Get shipment by shipment number.
      *
      * @param string $number
