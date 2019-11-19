0feb28bf82e3cdcb668d7bda73f7ae02c7b86108
Tomas Saghy <tomas.saghy@gmail.com>
2019-11-19 15:25:14 +0100

    added getShipment method

--- src/Tracker.php
+++ src/Tracker.php
@@ -37,12 +37,39 @@ class Tracker
     }
 
     /**
+     * Get shipment by shipment number.
+     *
+     * @param string $number
+     *
+     * @return \stdClass
+     */
+    public function getShipment(string $number): \stdClass
+    {
+        if (!\preg_match('/([0-9]{3})-([0-9]{3})-([0-9]+)/', $number, $m)) {
+            throw new \InvalidArgumentException('Invalid shipment number format!');
+        }
+
+        try {
+            $response = $this->soap->__call('getShipment', [
+                'landnr' => $m[1],
+                'mandnr' => $m[2],
+                'lfdnr' => $m[3],
+                'langi' => $this->language
+            ]);
+        } catch (\SoapFault $e) {
+            return [];
+        }
+
+        return $response;
+    }
+
+    /**
      * Get shipments by reference number.
      *
      * @param string $reference
      * @param string $date
      *
-     * @return array
+     * @return \stdClass[]
      */
     public function getShipments(string $reference, string $date = ''): array
     {
